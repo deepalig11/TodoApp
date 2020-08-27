7e3cd186d44f818bbba46b057f4029150aa15e9f
Deepali <t4snietdeepaligupta@gmail.com>
2020-08-27 22:45:45 +0530

    aded status

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
 import FlatList from 'flatlist-react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card'
+
 import './App.css';
 
 function App() {
 
-  const [todoarray] = useState([]);
+  const [todoarray, funcArray] = useState([]);
   const [xyz, abc] = useState('');
   const pusharray = (event) => {
     event.preventDefault();
-    console.log(xyz);
-    // list.push({ task: xyz });
-    todoarray.push({ key: Math.random(), task: xyz });
-    // todoarray = list;
-    // localStorage.setItem(todoarray);
-    console.log(todoarray);
-    abc('');
+    if (xyz.length !== 0) {
+      funcArray([{ key: Math.random(), task: xyz, status: 0 }, ...todoarray]);
+      console.log(todoarray);
+      abc('');
+    }
   };
   const myChangeHandler = (event) => {
     abc(event.target.value);
-
-
   }
   const renderList = (list) => {
     return (
@@ -30,6 +28,15 @@ function App() {
     );
   }
 
+  const changeStatus = (x) => {
+    const elementsIndex = todoarray.findIndex(element => element.key === x)
+    console.log(elementsIndex);
+    let newArray = [...todoarray];
+    newArray[elementsIndex] = { key: newArray[elementsIndex].key, task: newArray[elementsIndex].task, status: 1 }
+    funcArray([...newArray]);
+    // todoarray[elementsIndex].status = 1;
+    console.log(todoarray);
+  }
 
 
 
@@ -44,20 +51,18 @@ function App() {
           <input id="todo" type="text" value={xyz} onChange={myChangeHandler} />
           <button onClick={pusharray}>Save</button>
         </form>
-        {/* <FlatList
-          list={todoarray}
-          renderItem={renderList}
-          renderWhenEmpty={() => <div>No task available</div>}
-        /> */}
         <ListGroup>
           {todoarray.map(item => {
             return (
-
               <ListGroup.Item variant="dark"
-              // onClick={() => this.deleteItem(item.id)}
+                onClick={() => { changeStatus(item.key) }}
               >
-                {todoarray.value}
+                <Card className="cardlist">
+
+                  {item.task}
+                </Card>
               </ListGroup.Item>
+
             )
           })}
         </ListGroup>
