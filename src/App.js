3828a6d0d0c52028a8c9dd337014bfcb2050a9e8
Deepali <t4snietdeepaligupta@gmail.com>
2020-08-31 00:27:39 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,66 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import List from './component/List/List';
 import Header from './component/Header/Header';
 
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoarray: [],
+      text: '',
+      tag: '',
+      tagArray: []
+    }
 
-  const [todoarray, funcArray] = useState([]);
-  const [text, setText] = useState('');
-  const [tag, setTag] = useState('');
-  const [tagArray, setTagArray] = useState([]);
+  }
 
-  const pusharray = (event) => {
-    event.preventDefault();
-    if (text.length !== 0) {
+  pusharray = (event) => {
+    if (this.state.text.length !== 0) {
 
       if (localStorage.getItem("record") == null) {
         const list = [];
-        list.push({ key: Math.random(), task: text, status: 0, });
+        list.push({ key: Math.random(), task: this.state.text, status: 0, });
         localStorage.setItem("record", JSON.stringify(list));
       } else {
         const list1 = JSON.parse(localStorage.getItem("record"));
-        list1.unshift({ key: Math.random(), task: text, status: 0, });
+        list1.unshift({ key: Math.random(), task: this.state.text, status: 0, });
         localStorage.setItem("record", JSON.stringify(list1));
       }
 
-      // todoarray.unshift({ key: Math.random(), task: text, status: 0, })
-      funcArray(JSON.parse(localStorage.getItem("record")));
-      // console.log('task added', todoarray);
-      setText('');
+      this.setState({
+        todoarray: (JSON.parse(localStorage.getItem("record"))),
+        text: ''
+      })
     }
   };
 
 
-  const myTagHandler = (event) => {
-    setTag(event.target.value);
-    // const str = (event.target.value).toLowerCase();
-    // let list1 = [];
-    // if (event.target.value === null) {
-    //   list1 = JSON.parse(localStorage.getItem("record"));
-    // }
-    // else {
-    //   const list = JSON.parse(localStorage.getItem("record"));
-    //   list1 = list.filter(item => {
-    //     const l1 = item.task.toLowerCase();
-    //     return l1.includes(str);
-    //   });
-    // }
-    // funcArray(list1);
+  myTagHandler = (event) => {
+
+    this.setState({ tag: event.target.value })
   }
 
-  useEffect(() => {
+  componentDidMount() {
     const list = JSON.parse(localStorage.getItem('record'));
     if (list !== null)
-      funcArray(list);
-  }, [])
+      this.setState({ todoarray: list })
+  }
 
-  const changeStatus = (x) => {
-    const elementsIndex = todoarray.findIndex(element => element.key === x)
+  changeStatus = (x) => {
+    const elementsIndex = this.state.todoarray.findIndex(element => element.key === x)
 
-    if (todoarray[elementsIndex].status === 0) {
+    if (this.state.todoarray[elementsIndex].status === 0) {
 
       let i;
       if (localStorage.getItem("counter") == null) {
@@ -71,86 +63,77 @@ function App() {
         i = i + 1;
         localStorage.setItem("counter", JSON.stringify(i));
       }
-      let newArray = [...todoarray];
+      let newArray = [...this.state.todoarray];
       newArray[elementsIndex] = { key: newArray[elementsIndex].key, task: newArray[elementsIndex].task, status: i }
-      funcArray(newArray);
-      console.log(todoarray);
+      this.setState({ todoarray: newArray })
       localStorage.setItem("record", JSON.stringify(newArray));
     }
   }
 
-  const resetList = () => {
+  resetList = () => {
     localStorage.clear();
-    funcArray([]);
+    this.setState({ todoarray: [] })
   }
 
-  const hashTag = (event) => {
-    event.preventDefault();
-    if (tag.length !== 0) {
-      const arr = [...tagArray];
-      arr.push({ tagValue: tag })
-      setTagArray(arr);
-      setTag('');
+  hashTag = (event) => {
+    if (this.state.tag.length !== 0) {
+      const arr = [...this.state.tagArray];
+      arr.push({ tagValue: this.state.tag })
+      this.setState({ tagArray: arr, tag: '' })
     }
-
-    console.log(tagArray);
   };
 
-  const textHandler = (event) => {
-    setText(event.target.value);
+  textHandler = (event) => {
+    this.setState({ text: event.target.value })
   }
 
-  const showFilteredTask = (str) => {
+  showFilteredTask = (str) => {
     document.getElementById(str).style.backgroundColor = "rgb(128, 0, 128)";
     str = str.toLowerCase();
     console.log(str);
 
-    const list = [...todoarray];
+    const list = [...this.state.todoarray];
     const list1 = list.filter(item => {
       const l1 = item.task.toLowerCase();
       return l1.includes(str);
     });
 
-    funcArray(list1);
+    this.setState({ todoarray: list1 })
 
   }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header
+            resetList={this.resetList}
+            pusharray={this.pusharray}
+            myTagHandler={this.myTagHandler}
+            hashTag={this.hashTag}
+            textHandler={this.textHandler}
+            text={this.state.text}
+            tag={this.state.tag}
+          />
+          {
+            this.state.tagArray.map((item) => {
+              return (
+                <button id={item.tagValue} onClick={() => { this.showFilteredTask(item.tagValue) }} >{item.tagValue}</button>
+              );
 
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header
-          resetList={resetList}
-          pusharray={pusharray}
-          myTagHandler={myTagHandler}
-          hashTag={hashTag}
-          textHandler={textHandler}
-          text={text}
-          tag={tag}
-        />
-        {
-          tagArray.map((item) => {
-            return (
-              <button id={item.tagValue} onClick={() => { showFilteredTask(item.tagValue) }} >{item.tagValue}</button>
-            );
-
-          })
-        }
-        <List array={todoarray.filter(items => items.status === 0)}
-          changeStatus={changeStatus}
-          word={tag} />
-        <h2 style={{ textAlign: "center" }}>Completed</h2>
-        <List array={todoarray.filter(item => item.status !== 0).sort((a, b) => { return b.status - a.status })}
-          changeStatus={changeStatus}
-          word={tag} />
-
-
-
-      </header>
-    </div >
-  );
+            })
+          }
+          <List array={this.state.todoarray.filter(items => items.status === 0)}
+            changeStatus={this.changeStatus}
+            word={this.state.tag} />
+          <h2 style={{ textAlign: "center" }}>Completed</h2>
+          <List array={this.state.todoarray.filter(item => item.status !== 0).sort((a, b) => { return b.status - a.status })}
+            changeStatus={this.changeStatus}
+            word={this.state.tag} />
+        </header>
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,17 +3,18 @@ import { Button } from 'react-bootstrap';
 
 const Header = (props) => {
     return (
-        <>
-            <Button variant="danger" onClick={props.resetList}>Reset</Button>
-            <h1 style={{ textAlign: "center" }} >TO-DO Application</h1>
+        <div>
+            <Button variant="secondary" onClick={props.resetList} style={{ float: 'right' }}>Reset</Button>
+            <input id="createHashtags" type="text" value={props.tag} placeholder="Add Hashtag" onChange={props.myTagHandler} style={{ float: 'left' }} />
+            <button onClick={props.hashTag} style={{ float: 'left' }}>Create Hashtag</button>
+            <br />
+            <h1 style={{ textAlign: "center" }}>TO-DO Application</h1>
             <form style={{ textAlign: "center" }}>
-                <input id="todo" type="text" value={props.text} onChange={props.textHandler} />
-                <button onClick={props.pusharray}>Save</button>
-                <br />
-                <input id="createHashtags" type="text" value={props.tag} onChange={props.myTagHandler} style={{ float: 'left' }} />
-                <button onClick={props.hashTag} style={{ float: 'left' }}>Create Hashtag</button>
+                <input id="todo" type="text" value={props.text} placeholder="Add Task" onChange={props.textHandler} />
+                <button onClick={props.pusharray}>Save Task</button>
             </form>
-        </>
+
+        </div >
 
     );
 
